2673ff6e4c3a08ecb0af2ef0d07d399feb2febc7
samuel-owie <owiesamuelehizogie@gmail.com>
2025-01-05 23:48:05 -0500

    update to the stock class

--- StockTrackerV1/src/Stock.java
+++ StockTrackerV1/src/Stock.java
@@ -1,2 +1,16 @@
 public class Stock {
+    private String symbol;
+    private String name;
+    private double price;
+    private int quantity;
+
+    //Constructor
+    public Stock(String symbol, String name, double price, int quantity){
+        this.symbol = symbol;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    //Getters and Setters
 }
