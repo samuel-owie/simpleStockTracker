34fe5713fb91b4d6b914aeb533e4cb7a2a06f532
samuel-owie <owiesamuelehizogie@gmail.com>
2025-01-06 00:17:26 -0500

    Updates to the Main class

--- StockTrackerV1/src/Main.java
+++ StockTrackerV1/src/Main.java
@@ -3,5 +3,12 @@ public class Main {
 
         //create the stock tracker
         StockTracker tracker = new StockTracker();
+
+        //Stocks to be tracked
+        tracker.addStock(new Stock("AAPL", "Apple Inc.", 150.00, 50));
+        tracker.addStock(new Stock("GOOGL", "Alphabet Inc.", 2800.00, 10));
+        tracker.addStock(new Stock("TSLA", "Tesla Inc.", 700.00, 20));
+
+        //Display all stocks
     }
 }
